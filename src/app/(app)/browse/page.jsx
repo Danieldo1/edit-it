77f07051d5ea0a06f28a9b88dd09fb2a4cb62f8b
Dan <daniel.speranskiy@live.com>
2024-01-24 20:27:59 +0400

    Added a grid for all screensizes

--- src/app/(app)/browse/page.jsx
+++ src/app/(app)/browse/page.jsx
@@ -17,17 +17,20 @@ const fetchImg = async () => {
 })
 }
 const gridClasses = [
-  "col-span-2 row-span-5",                          // 1
-  "row-span-3 col-start-3",                        // 2
-  "row-span-3 col-start-4",                        // 3
-  "row-span-3 col-start-3 row-start-4",             // 4
-  "row-span-3 col-start-4 row-start-4",            // 5
-  "col-span-2 row-span-4 col-start-3 row-start-7", // 6
-  "col-span-2 row-span-5 col-start-1 row-start-6"  // 7
+  "col-span-4 row-span-4 md:col-span-6 md:row-span-6",            // For div 1
+  "col-span-2 row-span-2 col-start-5 md:col-span-3 md:row-span-3 md:col-start-7", // For div 2
+  "col-span-2 row-span-2 col-start-5 row-start-3 md:col-span-3 md:row-span-3 md:col-start-10", // For div 3
+  "col-span-4 row-span-4 col-start-3 row-start-5 md:col-span-3 md:row-span-3 md:col-start-7 md:row-start-4", // For div 4
+  "col-span-2 row-span-2 col-start-1 row-start-5 md:col-span-3 md:row-span-3 md:col-start-10 md:row-start-4", // For div 5
+  "col-span-2 row-span-2 row-start-7 md:col-span-3 md:row-span-3 md:row-start-7", // For div 6
+  "col-span-2 row-span-2 row-start-9 md:col-span-3 md:row-span-3 md:col-start-4 md:row-start-7", // For div 7
+  "col-span-2 row-span-2 col-start-3 row-start-9 md:col-span-3 md:row-span-3 md:col-start-1 md:row-start-10", // For div 8
+  "col-span-2 row-span-2 col-start-5 row-start-9 md:col-span-3 md:row-span-3 md:col-start-4 md:row-start-10", // For div 9
+  "md:col-span-6 md:row-span-6 md:col-start-7 md:row-start-7",
   ];
   return (
     <section className="w-full h-full mb-10">
-      <div className="grid grid-cols-4 grid-rows-10 gap-1 md:gap-5">
+      <div className="grid grid-cols-6 grid-rows-10 gap-4 md:grid-cols-12 md:grid-rows-12 ">
       {imageData.length > 0 && imageData.map((image, index) => (
         <img
           key={image._id}
--- src/app/api/retrive/route.js
+++ src/app/api/retrive/route.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 export async function GET() {
     mongoose.connect(process.env.MONGODB_URI)
-    const data = await File.find()
+    const data = await File.find().sort({createdAt: -1})
     // console.log(data, 'data')
     return Response.json(data)
 }
