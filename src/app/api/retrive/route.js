0ccd8a015175e70d9f3e3bcebcc74c12564d2ee2
Dan <daniel.speranskiy@live.com>
2024-01-26 13:39:26 +0400

    Fixed mongodb get

--- src/app/api/retrive/route.js
+++ src/app/api/retrive/route.js
@@ -3,8 +3,8 @@ import {File} from "@/models/File"; // Ensure this points to your Mongoose model
 import mongoose from 'mongoose';
 
 export async function GET() {
-    mongoose.connect(process.env.MONGODB_URI)
+   await mongoose.connect(process.env.MONGODB_URI)
     const data = await File.find().sort({createdAt: -1})
-    // console.log(data, 'data')
+    console.log(data, 'data')
     return Response.json(data)
 }
