7dd67cb3e7c2aabacd881d53a2125679c81ef58c
Dan <daniel.speranskiy@live.com>
2024-01-28 12:27:36 +0400

    Fixing mongo get 9

--- src/app/api/retrive/route.js
+++ src/app/api/retrive/route.js
@@ -4,16 +4,17 @@ import mongoose from 'mongoose'
 import {NextResponse} from 'next/server'
 
 export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
 
 export async function GET() {
     const data = await File.find()
     mongoose.connect(process.env.MONGODB_URI)
-    const headers = {
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-        Pragma: 'no-cache',
-        Expires: '0',
+    // const headers = {
+    //     'Cache-Control': 'no-cache, no-store, must-revalidate',
+    //     Pragma: 'no-cache',
+    //     Expires: '0',
 
-      };
+    //   };
      // console.log(data, 'data')
-    return new NextResponse(JSON.stringify(data), { headers })
+    return NextResponse.json(data)
 }
