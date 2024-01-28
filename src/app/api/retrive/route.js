9ed7eaee1ed2d72146ce05436ec3e3602ad6899c
Dan <daniel.speranskiy@live.com>
2024-01-28 10:53:22 +0400

    Fixing mongo get

--- src/app/api/retrive/route.js
+++ src/app/api/retrive/route.js
@@ -5,6 +5,11 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     const data = await File.find({}).sort({createdAt: -1})
+    const headers = {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        Pragma: 'no-cache',
+        Expires: '0'
+      };
      // console.log(data, 'data')
-    return NextResponse.json(data)
+    return new NextResponse(JSON.stringify(data), { headers })
 }
