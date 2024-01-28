88fa235e7a2bb36ce0e4a2f32993a86446786820
Dan <daniel.speranskiy@live.com>
2024-01-28 12:10:42 +0400

    Fixing mongo get 7

--- src/app/api/retrive/route.js
+++ src/app/api/retrive/route.js
@@ -3,6 +3,8 @@ import {File} from "../../../models/File";
 import mongoose from 'mongoose'
 import {NextResponse} from 'next/server'
 
+export const maxDuration = 15000
+export const runtime = 'nodejs'
 
 export async function GET() {
     const data = await File.find()
@@ -10,7 +12,8 @@ export async function GET() {
     const headers = {
         'Cache-Control': 'no-cache, no-store, must-revalidate',
         Pragma: 'no-cache',
-        Expires: '0'
+        Expires: '0',
+
       };
      // console.log(data, 'data')
     return new NextResponse(JSON.stringify(data), { headers })
